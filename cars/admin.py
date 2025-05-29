c3aef9ec889da433f414af6ab5f063a66c9c3f5a
Anthony Martins <128435292+Anthonyysm@users.noreply.github.com>
2025-05-29 14:21:00 -0300

    feat: templates

--- cars/admin.py
+++ cars/admin.py
@@ -4,7 +4,10 @@ from cars.models import Car, Brand
 
 class CarAdmin(admin.ModelAdmin):
     list_display = ('model', 'brand', 'factory_year', 'model_year', 'value')
-    search_fields = ('model', 'brand',)
+    search_fields = (
+        'model',
+        'brand',
+    )
 
 
 class BrandAdmin(admin.ModelAdmin):
--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from cars.models import Car
 
-# Create your views here.
+
+def cars_view(request):
+    cars = Car.objects.all()
+    print(request.GET.get('search'))
+
+    return render(
+        request,
+        'cars.html',
+        {'cars': cars}
+    )
