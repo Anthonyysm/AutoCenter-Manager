2aff8bbe10e586d7c7fceef0220444860e2e2f4a
Anthony Martins <128435292+Anthonyysm@users.noreply.github.com>
2025-10-01 16:59:33 -0300

    feat: login, logout

--- cars/forms.py
+++ cars/forms.py
@@ -12,19 +12,26 @@ class CarModelForm(forms.ModelForm):
         value = self.cleaned_data.get('value')
         if value < 15000:
             self.add_error(
-                'value', 'Valor mínimo do carro deve ser de R$15.000')
+                'value', 'Valor mínimo do carro deve ser de R$15.000'
+            )
         return value
 
     def clean_factory_year(self):
         factory_year = self.cleaned_data.get('factory_year')
         if factory_year < 1975:
             self.add_error(
-                'factory_year', 'Carros com ano de lançamento inferior a 1975 não são aceitos')
+                'factory_year',
+                'Carros com ano de lançamento inferior a 1975 não são aceitos',
+            )
         return factory_year
 
     def clean_plate(self):
         plate = self.cleaned_data.get('plate')
-        if not match(r'^[A-Z]{3}-[0-9]{4}', plate) and not match(r'[A-Z]{3}[0-9]{1}[A-Z]{1}[0-9]{2}', plate):
+        if not match(r'^[A-Z]{3}-[0-9]{4}', plate) and not match(
+            r'[A-Z]{3}[0-9]{1}[A-Z]{1}[0-9]{2}', plate
+        ):
             self.add_error(
-                'plate', 'Formato de placa inválido! Digite no formato brasileiro (AAA-0000) ou no formato mercosul (AAA0A00)')
+                'plate',
+                'Formato de placa inválido! Digite no formato brasileiro (AAA-0000) ou no formato mercosul (AAA0A00)',
+            )
         return plate
