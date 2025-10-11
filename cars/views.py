c0c04192f5f66076d1f12477a709b74ea006efb0
Anthony Martins <128435292+Anthonyysm@users.noreply.github.com>
2025-10-11 11:52:01 -0300

    refactor(cars): Reescrita da view para Cars List

--- cars/views.py
+++ cars/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views import View
+from django.views.generic import ListView
 from cars.models import Car
 from cars.forms import CarModelForm
 
@@ -15,6 +16,17 @@ class CarsView(View):
 
         return render(request, 'cars.html', {'cars': cars})
 
+class CarsListView(ListView):
+    model = Car
+    template_name = 'cars.html'
+    context_object_name = 'cars'
+
+    def get_queryset(self):
+        cars = super().get_queryset().order_by('model')
+        search = self.request.GET.get('search')
+        if search:
+            cars = cars.filter(model__icontains=search)
+        return cars
 
 class NewCarView(View):
 
