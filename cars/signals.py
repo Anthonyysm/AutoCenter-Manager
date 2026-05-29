28dcba38044d542ebefd6ae7e33aa9e4250f11a1
Anthony Martins <128435292+Anthonyysm@users.noreply.github.com>
2026-05-29 08:12:34 -0300

    feat(signals): post save add

--- cars/signals.py
+++ cars/signals.py
@@ -1,19 +1,16 @@
-from django.db.models.signals import pre_save, pre_delete, post_delete, post_save
+from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
 from cars.models import Car
+from django.db.models import Sum
 
-
-@receiver(pre_save, sender=Car)
-def car_pre_save(sender, instance, **kwargs):
-    ...
 
 @receiver(post_save, sender=Car)
 def car_post_save(sender, instance, **kwargs):
-    ...
+    cars_caunt = Car.objects.all().count()
+    cars_value = Car.objects.aggregate(
+        total_value = Sum('value')
+    )
 
-@receiver(pre_delete, sender=Car)
-def car_pre_delete(sender, instance, **kwargs):
-    ...
 
 @receiver(post_delete, sender=Car)
 def car_post_delete(sender, instance, **kwargs):
