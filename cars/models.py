9a8a87793b71c043d4b0193d5d83a496c5d8b0e5
Anthony Martins <128435292+Anthonyysm@users.noreply.github.com>
2026-06-02 15:26:32 -0300

    feat(models): Add bio field to Car and show on detail

--- cars/models.py
+++ cars/models.py
@@ -20,6 +20,7 @@ class Car(models.Model):
     plate = models.CharField(max_length=16, blank=True, null=True)
     value = models.FloatField(blank=True, null=True)
     photo = models.ImageField(upload_to='cars/', blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return f'{self.brand} {self.model} ({self.model_year})'
